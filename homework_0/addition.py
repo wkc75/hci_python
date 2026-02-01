f1ae405d7c4ef95f13045b80d34187bd8fe76f89
wkc75 <wongkinchong75@gmail.com>
2026-02-01 02:27:27 +0000

    my first func

--- homework_0/addition.py
+++ homework_0/addition.py
@@ -1,7 +1,9 @@
-x = int(input("Key in the 1st number: "))
+def add():
+    x = int(input("Key in the 1st number: "))
 
-y = int(input("key in the 2nd number: "))
+    y = int(input("key in the 2nd number: "))
 
-a = x + y
+    a = x + y
 
-print(f"The sum of {x} and {y} is {a}")
+    print(f"The sum of {x} and {y} is {a}")
+
